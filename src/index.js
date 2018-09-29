abc25fe91255aca28b814c517ee45586eaa0b9e0
duza <duza@tut.by>
2018-09-29 23:56:04 +0300

    Add a solution of money exhange
    Iterate by face value and determine needed number of them.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
   const result = {};
-  if (currency > 10000) {
+  if (currency < 1) {
+    return result;
+  } else if (currency > 10000) {
     result.error = 'You are rich, my friend! We don\'t have so much coins for exchange';
+  } else {
+    const keys = ['H', 'Q', 'D', 'N', 'P'];
+    const values = [50, 25, 10, 5, 1];
+    let currentRemainder = currency;
+    keys.forEach((key, index) => {
+      const faceValue = values[index];
+      const numberCoinsOfKey = Math.floor(currentRemainder / faceValue);
+      if (numberCoinsOfKey) {
+        result[key] = numberCoinsOfKey;
+        currentRemainder -= numberCoinsOfKey * faceValue;
+      }
+    });
   }
-  // return {'H': 50, 'Q': 25, 'D': 10, 'N': 5, 'P': 1};
   return result;
 }
